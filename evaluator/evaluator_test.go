03936601fcb4df3651e6041b744e3f529d07ab72
andcov <andreicovaci123@gmail.com>
2021-05-24 18:29:25 +0300

    Add let statements and identifier evaluation

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -142,8 +142,9 @@ func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
 	program := p.ParseProgram()
+	env := object.NewEnvironment()
 
-	return Eval(program)
+	return Eval(program, env)
 }
 
 func TestBangOperator(t *testing.T) {
@@ -231,6 +232,10 @@ if (10 > 1) {
 `,
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
+		{
+			"foobar",
+			"identifier not found: foobar",
+		},
 	}
 
 	for _, tt := range tests {
@@ -245,5 +250,21 @@ if (10 > 1) {
 		if errObj.Message != tt.expectedMessage {
 			t.Errorf("wrong error message. want=%s, got=%s", tt.expectedMessage, errObj.Message)
 		}
+	}
+}
+
+func TestLetStatements(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let a = 5; a;", 5},
+		{"let a = 5 * 5; a;", 25},
+		{"let a = 5; let b = a; b;", 5},
+		{"let a = 5; let b = a; let c = a + b + 5; c;", 15},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
 	}
 }
